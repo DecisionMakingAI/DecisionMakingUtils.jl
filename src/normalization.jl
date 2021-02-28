4b54a1bbd70115e0d49cbf31aca2ff1f6860648b
Scott Jordan <scott.m.jordan91@gmail.com>
2021-02-28 18:51:54 -0500

    Made normalizers able to adapt to incoming data

--- src/normalization.jl
+++ src/normalization.jl
@@ -1,49 +1,111 @@
-struct LinearNormalization{T} <: Any where {T}
-    a::T
-    b::T
+struct ZeroOneNormalization{T} <: Any where {T}
+    low::T
+    high::T 
 
-    function LinearNormalization(a::T, b::T) where {T}
-        return new{T}(deepcopy(a), deepcopy(b))
+    function ZeroOneNormalization(ranges)
+        low = ranges[:, 1]
+        high = ranges[:, 2]
+        return new{typeof(low)}(low, high)
+    end
+
+    function ZeroOneNormalization(low::T, high::T) where {T}
+        return new{T}(deepcopy(low), deepcopy(high))
     end
 end
 
+struct PosNegNormalization{T} <: Any where {T}
+    low::T
+    high::T 
 
-function ZeroOneNormalization(ranges)
-    range = ranges[:, 2] - ranges[:, 1]
-    a = ranges[:, 1]
-    b = convert(eltype(range), 1.0) ./ range
-    return LinearNormalization(a, b)
+    function PosNegNormalization(ranges)
+        low = ranges[:, 1]
+        high = ranges[:, 2]
+        return new{typeof(low)}(low, high)
+    end
+
+    function PosNegNormalization(low::T, high::T) where {T}
+        return new{T}(deepcopy(low), deepcopy(high))
+    end
 end
 
-function PosNegNormalization(ranges)
-    range = ranges[:, 2] - ranges[:, 1]
-    T = eltype(range)
-    a = ranges[:, 1] .+ convert(T, 0.5) .* range
-    b = convert(T, 2.0) ./ range
-    return LinearNormalization(a, b)
+struct GaussianNormalization{T} <: Any where {T}
+    v::T
+
+    function GaussianNormalization(num_features::Int)
+        v = num_features*KahanVariance()
+        return new{typeof(v)}(v)
+    end
+
+    function GaussianNormalization(num_features::Int, weight::T) where {T<:Real}
+        v = num_features*KahanVariance(weight=Exponential(weight))
+        return new{typeof(v)}(v)
+    end
+
+    function GaussianNormalization(num_features::Int, weight::OnlineStats.Weight)
+        v = num_features*KahanVariance(weight=weight)
+        return new{typeof(v)}(v)
+    end
 end
 
-# 2 (x - l) / r - 1
-# 2 (x - l) / r - r/r
-# (2x - 2l - r) / r
-# 2(x - l - 0.5r) / r
-
-function GaussianNormalization(μ, σ)
-    a = μ
-    b = convert(eltype(a), 1.0) ./ σ
-    println("a ",a )
-    println("b ", b)
-    return LinearNormalization(a, b)
-end
-
-function (f::LinearNormalization)(x)
+function (f::ZeroOneNormalization)(x; fit=false)
+    if fit
+        @. f.low = min(x, f.low)
+        @. f.high = max(x, f.high)
+    end
     y = zero(x)
-    @. y = (x - f.a) * f.b
+    @. y = (x - f.low) / (f.high - f.low)
     return y
 end
 
-function (f::LinearNormalization)(buff::T, x::T) where {T}
-    @. buff = (x - f.a) * f.b
+function (f::ZeroOneNormalization)(buff::T, x::T; fit=false) where {T}
+    if fit
+        @. f.low = min(x, f.low)
+        @. f.high = max(x, f.high)
+    end
+    @. buff = (x - f.low) / (f.high - f.low)
     return buff
 end
 
+function (f::PosNegNormalization)(x; fit=false)
+    if fit
+        @. f.low = min(x, f.low)
+        @. f.high = max(x, f.high)
+    end
+    y = zero(x)
+    T2 = eltype(x)
+    a = convert(T2, 2.0)
+    b = convert(T2, 1.0)
+    @. y = a * (x - f.low) / (f.high - f.low) - b
+    return y
+end
+
+function (f::PosNegNormalization)(buff::T, x::T; fit=false) where {T}
+    if fit
+        @. f.low = min(x, f.low)
+        @. f.high = max(x, f.high)
+    end
+    T2 = eltype(x)
+    a = convert(T2, 2.0)
+    b = convert(T2, 1.0)
+    @. buff = a * (x - f.low) / (f.high - f.low) - b
+    return buff
+end
+
+function (f::GaussianNormalization)(x; fit=false)
+    if fit
+        fit!(f.v, x)
+    end
+    y = zero(x)
+    T2 = eltype(x)
+    @. y = (x - convert(T2, mean(f.v.stats))) / convert(T2, std(f.v.stats))
+    return y
+end
+
+function (f::GaussianNormalization)(buff::T, x::T; fit=false) where {T}
+    if fit
+        fit!(f.v, x)
+    end
+    T2 = eltype(x)
+    @. buff = (x - convert(T2, mean(f.v.stats))) / convert(T2, std(f.v.stats))
+    return buff
+end
--- test/normalizationtest_constant.jl
+++ test/normalizationtest_constant.jl
@@ -1,5 +1,4 @@
 using DecisionMakingUtils
-using OnlineStats
 using Test
 
 @testset "ZeroOneNormalization Test" begin
@@ -25,17 +24,6 @@ using Test
     @test all(y .≤ 1.0)
     n(buff, high)
     @test all(isapprox.(buff, y))
-
-    @. low -= 1
-    @. high += 1
-    n(buff, low)
-    @test all(buff .< 0.0)
-    n(buff, low, fit=true)
-    @test all(buff .≥ 0.0)
-    n(buff, high)
-    @test all(buff .> 1.0)
-    n(buff, high, fit=true)
-    @test all(buff .≤ 1.0)
 end
 
 @testset "PosNegNormalization Test" begin
@@ -45,7 +33,7 @@ end
     high = collect(T, 1:k)
     ranges = hcat(low, high)
     n = PosNegNormalization(ranges)
-
+    
     x = zeros(T, k)
     buff = zero(x)
     y = n(x)
@@ -54,7 +42,7 @@ end
     @test all(y .≥ -1.0)
     @test all(y .≤ 1.0)
     y = n(low)
-    
+    println(low)
     @test all(y .≥ -1.0)
     @test all(y .≤ 1.0)
     y = n(high)
@@ -62,59 +50,24 @@ end
     @test all(y .≤ 1.0)
     n(buff, high)
     @test all(isapprox.(buff, y))
-
-    @. low -= 1
-    @. high += 1
-    n(buff, low)
-    @test all(buff .< -1.0)
-    n(buff, low, fit=true)
-    @test all(buff .≥ -1.0)
-    n(buff, high)
-    @test all(buff .> 1.0)
-    n(buff, high, fit=true)
-    @test all(buff .≤ 1.0)
 end
 
 @testset "GaussianNormalization Test" begin
     k = 3
     T = Float32
-    x1 = collect(T, 1:k)
-    x2 = collect(T, 1:k) .* 2 
-    x3 = collect(T, 1:k) .* 3
-    
-    n = GaussianNormalization(k)
+    μ = collect(T, 1:k)
+    σ = collect(T, 1:k)
+    n = GaussianNormalization(μ, σ)
     
     x = zeros(T, k)
     buff = zero(x)
     y = n(x)
     @test typeof(y) == typeof(x)
     @test size(y) == size(x)
-    
+    println(y)
+    @test all(isapprox.(y, -1.0))
+    y = n(μ)
     @test all(isapprox.(y, 0.0))
-    @. x = 1
-    y = n(x)
-    @test all(isapprox.(y, 1.0))
-    
-    n(buff, x)
+    n(buff, μ)
     @test all(isapprox.(buff, y))
-
-    v = k*KahanVariance()
-    x1 = collect(T, 1:k)
-    x2 = collect(T, 1:k) .* 2 
-    x3 = collect(T, 1:k) .* 3
-
-    fit!(v, x1)
-    @. y = (x1 - mean.(v.stats)) / std.(v.stats)
-    n(buff, x1, fit=true)
-    @test all(isapprox(buff, y))
-
-    fit!(v, x2)
-    @. y = (x2 - mean.(v.stats)) / std.(v.stats)
-    n(buff, x2, fit=true)
-    @test all(isapprox(buff, y))
-
-    fit!(v, x3)
-    @. y = (x3 - mean(v.stats)) / std(v.stats)
-    n(buff, x3, fit=true)
-    @test all(isapprox(buff, y))
 end
