5719aff04867832a7066425fb5bd4ef96534a5f7
Scott <scott.m.jordan91@gmail.com>
2021-06-24 09:07:43 -0400

    fixed doc string for fourier basis

--- src/basis/fourierbasis.jl
+++ src/basis/fourierbasis.jl
@@ -3,7 +3,7 @@
 
 Creates a struct to generate fourier features up to a given order. Both coupled fourier features, e.g., 
 ``\\cos(π(3x₁ + 2x₂))`` and uncoupled features, ``[\\cos(πx₁), \\cos(π2x₁), …]`` can be generated with this basis function. 
-The dorder parameter controls order for the coupling features. The number of coupled features generated, ``(\\text{num\\_inputs}+1)^{\\text{dorder}}``,
+The dorder parameter controls order for the coupling features. The number of coupled features generated, ``(\\text{dorder}+1)^{\\text{num\\_inputs}}``,
  grows exponentially with dorder, so it is not reccomended for use with high deminsional vectors.
 The iorder parameter controls the order of the independent features generated. The full parameter determines if both 
 ``\\sin`` and ``\\cos`` features are generated, if false only cos features are generated. 
