f89687af1a417d683ee03e95d306758cf300f487
Scott Jordan <scott.m.jordan91@gmail.com>
2023-05-30 16:48:36 -0400

    fixed tile coding and added new constructor

--- src/basis/tilecoding.jl
+++ src/basis/tilecoding.jl
@@ -1,12 +1,9 @@
 """
-    TileCodingBasis([::Type,] num_inputs::Int, num_tiles::Int)
+    TileCodingBasis(num_inputs::Int, num_tiles::Int)
 
-Creates a struct to generate fourier features up to a given order. Both coupled fourier features, e.g., 
-``\\cos(π(3x₁ + 2x₂))`` and uncoupled features, ``[\\cos(πx₁), \\cos(π2x₁), …]`` can be generated with this basis function. 
-The dorder parameter controls order for the coupling features. The number of coupled features generated, ``(\\text{dorder}+1)^{\\text{num\\_inputs}}``,
- grows exponentially with dorder, so it is not reccomended for use with high deminsional vectors.
-The iorder parameter controls the order of the independent features generated. The full parameter determines if both 
-``\\sin`` and ``\\cos`` features are generated, if false only cos features are generated. 
+Creates a tile coding basis with `num_inputs` inputs and `num_tiles` tiles per input. 
+Alternatively, construction can just specify `num_tiles` as a vector of integers, specifying the number of tiles per input.
+The tiles are spread equally across [0,1] for each input. The basis is implemented as a sparse vector, where each element is 1 if the input is in the corresponding tile, and 0 otherwise.
 
 See also: [`FourierBasisBuffer`](@ref)
 
@@ -15,16 +12,21 @@ See also: [`FourierBasisBuffer`](@ref)
 ```jldoctest
 julia> f = TileCodingBasis(2, 3);
 
-julia> x = [0.0, 0.5];
+julia> x = [0.0, 0.0];
 
 julia> feats = f(x)
-6-element Array{Float64,1}:
-  1.0
-  6.123233995736766e-17
-  1.0
-  6.123233995736766e-17
-  1.0
- -1.0
+1
+
+julia> feats = f([1,1])
+9
+
+julia> length(f)
+9
+
+julia> f = TileCodingBasis([10,10,3]);
+
+julia> length(f)
+300
 
 ```
 """
@@ -34,19 +36,32 @@ struct TileCodingBasis{T,TL,TC} <: Any where {T,TL,TC}
     lidxs::TL
     cidxs::TC
     function TileCodingBasis(num_inputs::Int, num_tiles::Int)
-        bins = 0:1/num_tiles:1
+        bins = range(0.0, stop=1.0, length=num_tiles)
         sz = ntuple(i->length(bins), num_inputs)
         lidxs = LinearIndices(sz)
         cidxs = CartesianIndices(sz)
         new{typeof(bins), typeof(lidxs), typeof(cidxs)}(bins, num_inputs, lidxs, cidxs)
     end
+
+    function TileCodingBasis(num_tiles::Vector{Int})
+        bins = [range(0.0, stop=1.0, length=n) for n in num_tiles]
+        sz = ntuple(i->length(bins[i]), length(num_tiles))
+        lidxs = LinearIndices(sz)
+        cidxs = CartesianIndices(sz)
+        new{typeof(bins), typeof(lidxs), typeof(cidxs)}(bins, length(num_tiles), lidxs, cidxs)
+    end
 end
 
 
-function (ϕ::TileCodingBasis{T,<:LinearIndices{N}})(x) where {T,N}
+function (ϕ::TileCodingBasis{<:StepRangeLen})(x)
     n = length(ϕ.bins)
     f(i,x) = min(searchsortedfirst(ϕ.bins, x[i]), n)
-    return ϕ.lidxs[CartesianIndex(ntuple(i->f(i,x), Val(N)))]
+    return ϕ.lidxs[CartesianIndex(ntuple(i->f(i,x), ϕ.num_inputs))]
+end
+
+function (ϕ::TileCodingBasis{<:Vector{<:StepRangeLen}})(x)
+    f(i,x) = min(searchsortedfirst(ϕ.bins[i], x[i]), length(ϕ.bins[i]))
+    return ϕ.lidxs[CartesianIndex(ntuple(i->f(i,x), ϕ.num_inputs))]
 end
 
 """
@@ -55,7 +70,12 @@ end
 Returns the number of feautes produced by the tile coding basis. 
 """
 
-function length(ϕ::TileCodingBasis)
+function Base.length(ϕ::TileCodingBasis{<:StepRangeLen})
     n = length(ϕ.bins)
     return n^ϕ.num_inputs
 end
+
+function Base.length(ϕ::TileCodingBasis{<:Vector{<:StepRangeLen}})
+    return prod(length, ϕ.bins)
+end
+
